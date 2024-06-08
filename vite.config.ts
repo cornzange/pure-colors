ef51d6be0fae24bec9dc4fc9155a7ecd683add6a
cornzange <cornzange@gmail.com>
2024-06-08 16:18:37 +0300

    add firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +30,13 @@ export default defineConfig({
                 NodeGlobalsPolyfillPlugin({
                     buffer: true
                 })
+            ]
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                rollupNodePolyFill()
             ]
         }
     }
